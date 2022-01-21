ef905ddf38bdc2e0c62ccf9675829c893fae8e00
Yuri Ognyanov <yuriognqnov@gmail.com>
2022-01-22 01:12:34 +0200

    Asynchronously processing
    Make the Web Server run Asynchronously and add Download Web Content

--- HttpServer/BasicWebServer.Demo/StartUp.cs
+++ HttpServer/BasicWebServer.Demo/StartUp.cs
@@ -11,19 +11,63 @@ Age: <input type='number' name ='Age'/>
 <input type='submit' value ='Save' /> 
 </form>";
 
-    public static void Main()
-    => new HttpServer(routes => routes
-.MapGet("/", new TextResponse("Hello from the server!"))
-.MapGet("/Redirect", new RedirectResponse("https://softuni.org/"))
-.MapGet("/HTML", new HtmlResponse(StartUp.HtmlForm))
-.MapPost("/HTML", new TextResponse("", StartUp.AddFormDataAction)))        
-.Start();
+    private const string DownloadForm = @"<form action='/Content' method='POST'> 
+ <input type='submit' value ='Download Sites Content' />  
+</form>";
 
-    private static void AddFormDataAction(Request request, Response response) 
+    private const string FileName = "conntent.txt";
+
+    public static async Task Main()
+    {
+        await DownloadSitesAsTextFile(StartUp.FileName
+            , new string[] { "https://judge.softuni.org/", "https://softuni.org/" });
+
+        var server = new HttpServer(routes => routes
+            .MapGet("/", new TextResponse("Hello from the server!"))
+            .MapGet("/Redirect", new RedirectResponse("https://softuni.org/"))
+            .MapGet("/HTML", new HtmlResponse(StartUp.HtmlForm))
+            .MapPost("/HTML", new TextResponse("", StartUp.AddFormDataAction))
+            .MapGet("/Content", new HtmlResponse(StartUp.DownloadForm))
+            .MapPost("/Content", new TextFileResponse(StartUp.FileName)));
+            
+        await server.Start();
+    }
+
+
+    private static async Task DownloadSitesAsTextFile(string fileName, string[] urls)
+    {
+        var downloads = new List<Task<string>>();
+
+        foreach (var url in urls)
+        {
+            downloads.Add(DownloadWebSiteContent(url));
+        }
+
+        var responses = await Task.WhenAll(downloads);
+
+        var responsesString = string.Join("\r\n" + new string('-', 100), responses);
+
+        await File.WriteAllTextAsync(fileName, responsesString);
+    }
+
+    private static async Task<string> DownloadWebSiteContent(string url)
+    {
+        var httpClient = new HttpClient();
+        using (httpClient)
+        {
+            var response = await httpClient.GetAsync(url);
+
+            var html = await response.Content.ReadAsStringAsync();
+
+            return html.Substring(0, 2000);
+        }
+    }
+
+    private static void AddFormDataAction(Request request, Response response)
     {
         response.Body = "";
 
-        foreach (var (key,value) in request.Form)
+        foreach (var (key, value) in request.Form)
         {
             response.Body += $"{key} - {value}";
             response.Body += Environment.NewLine;
--- HttpServer/BasicWebServer.Server/HttpServer.cs
+++ HttpServer/BasicWebServer.Server/HttpServer.cs
@@ -38,7 +38,7 @@ namespace BasicWebServer.Server
         {
 
         }
-        private string ReadRequest(NetworkStream networkStream)
+        private async Task<string> ReadRequest(NetworkStream networkStream)
         {
             var bufferLength = 1024;
             var buffer = new byte[bufferLength];
@@ -49,7 +49,7 @@ namespace BasicWebServer.Server
 
             do
             {
-                var bytesRead = networkStream.Read(buffer, 0, bufferLength);
+                var bytesRead = await networkStream.ReadAsync(buffer, 0, bufferLength);
 
                 totalbytes += bytesRead;
 
@@ -64,7 +64,7 @@ namespace BasicWebServer.Server
 
             return requestBuilder.ToString();
         }
-        public void Start()
+        public async Task Start()
         {
             this.serverListener.Start();
 
@@ -73,34 +73,38 @@ namespace BasicWebServer.Server
 
             while (true)
             {
-                var connection = serverListener.AcceptTcpClient();
+                var connection = await serverListener.AcceptTcpClientAsync();
 
-                var networkStream = connection.GetStream();
-
-                var requestText = this.ReadRequest(networkStream);
-
-                Console.WriteLine(requestText);
-
-                var request = Request.Parse(requestText);
-
-                var response = this.routingTable.MatchRequest(request);
-
-                if (response.PreRenderAction != null)
+                _ = Task.Run(async () =>
                 {
-                    response.PreRenderAction(request, response);
-                }
+                    var networkStream = connection.GetStream();
 
-                WriteResponse(networkStream, response);
+                    var requestText = await this.ReadRequest(networkStream);
 
-                connection.Close();
+                    Console.WriteLine(requestText);
+
+                    var request = Request.Parse(requestText);
+
+                    var response = this.routingTable.MatchRequest(request);
+
+                    if (response.PreRenderAction != null)
+                    {
+                        response.PreRenderAction(request, response);
+                    }
+
+                    await WriteResponse(networkStream, response);
+
+                    connection.Close();
+                });
+
             }
         }
 
-        private void WriteResponse(NetworkStream networkStream, Response response)
+        private async Task WriteResponse(NetworkStream networkStream, Response response)
         {
             var responseBytes = Encoding.UTF8.GetBytes(response.ToString());
 
-            networkStream.Write(responseBytes);
+            await networkStream.WriteAsync(responseBytes);
         }
     }
 }
